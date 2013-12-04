8e988dfe186d4d5e6d886c4fe6d60ec1027ac8d1
UberLame <w0rm@sploit.me.uk>
2013-12-04 12:51:47 -0600

    index

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
-<?php include('config.php') ?>
 <?php
-$config = new config();
-@$data= $_REQUEST['code'];
-if(isset($data)&&!empty($data)){
+	include('config.php');
 
-$config->paste($data , $_SERVER['REMOTE_ADDR']);
+	$config = new config();
+	@$data= $_REQUEST['code'];
 
-}
-
-
+	if(isset($data)&&!empty($data))
+	{
+		$config->paste($data , $_SERVER['REMOTE_ADDR']);
+	}
 ?>
+
 <!doctype html>
 <html>
 <head>
@@ -27,16 +27,20 @@ $config->paste($data , $_SERVER['REMOTE_ADDR']);
 <br/><br/><br/>
 <div id='container'>
 <?php 
-if(isset($_GET['view_id'])){
-
-$row = $config->view($_GET['view_id']);
-echo 'Date posted: <b>',$row['date'],'</b><br/>'.'<div id=\'code\'><code>'.$row['paste'].'</code></div>';
-}else{?>
-<form action="<?=$_SERVER['PHP_SELF'];?>" method='POST'>
+	if(isset($_GET['view_id']))
+	{
+		$row = $config->view($_GET['view_id']);
+		echo 'Date posted: <b>',$row['date'],'</b><br/>'.'<div id=\'code\'><code>'.$row['paste'].'</code></div>';
+	}
+	else {
+?>
+<form action="index.php" method="POST">
 <textarea name='code'></textarea><br/>
 <input type='submit' value='paste'/>
 </form>
-<?}?>
+<?php
+	}
+?>
 </div>
 </center>
 </body>
